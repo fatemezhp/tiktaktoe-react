138cd6f4d72cfbe39629896dd742a429d03d1b10
Fatemezahra Habibpor <fatemeahbibpour@gmail.com>
2024-07-01 16:27:35 +0330

    the progect has started

---
+++ tik-tak-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Square from './componenets/Square'
+
+function App() {
+  return (
+    <div>
+      <Square/>
+    </div>
+  )
+}
+
+export default App
---
+++ tik-tak-toe/src/componenets/Square.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Square() {
+  return (
+    <button className="border-red-500 text-red-500">X</button>
+  )
+}
+
+export default Square
