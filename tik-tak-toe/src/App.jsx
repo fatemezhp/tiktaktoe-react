e2e606f494d54dc39316770b57659ab428c2bf61
Fatemezahra Habibpor <fatemeahbibpour@gmail.com>
2024-07-01 23:38:13 +0330

    new update

--- tik-tak-toe/src/App.jsx
+++ tik-tak-toe/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Square from './componenets/Square'
+import Board from './componenets/Board'
+
 
 function App() {
   return (
     <div>
-      <Square/>
+      <Board />
     </div>
   )
 }
---
+++ tik-tak-toe/src/componenets/Board.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Squre from './Square'
+
+function Board() {
+  return (
+    <>
+    <div>
+        <Squre />
+        <Squre />
+        <Squre />
+    </div>
+    <div>
+        <Squre />
+        <Squre />
+        <Squre />
+    </div>
+    <div>
+        <Squre />
+        <Squre />
+        <Squre />
+    </div>
+    </>
+  )
+}
+
+export default Board
